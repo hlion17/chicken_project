ee8d3ec15bf9dc6e5bbd6e99896d972d3341344c
hlion17 <yourdad20626@gmail.com>
2022-07-03 01:12:14 +0900

    front 3000 포트 cors 설정추가

---
+++ src/main/java/com/goaldenchicken/api/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.goaldenchicken.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.http.HttpMethod.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods(
+                        GET.name(),
+                        POST.name(),
+                        DELETE.name(),
+                        PATCH.name()
+                );
+    }
+}
